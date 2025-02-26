a5130816f01e8926c292643dea48b919d7dde24e
Laye <samblaye34@gmail.com>
2025-02-26 13:38:17 +0000

    Ajout d'une nouvelle fonctionnalité

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,7 @@ module com.banking.digitalbankingproject {
     requires javafx.fxml;
     requires java.sql;
     requires static lombok;
-    //requires TrayNotification; // Ensure the appropriate library is added as a dependency
+    requires TrayNotification;
     requires jbcrypt;
 
 
