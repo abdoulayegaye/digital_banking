08e7b8becc6549233ccf026ab28b58213457bbf3
Ngom <130662339+Cngom99@users.noreply.github.com>
2025-03-06 03:12:49 +0000

    ClientController.java

--- src/main/java/com/banking/digitalbankingproject/controller/ClientController.java
+++ src/main/java/com/banking/digitalbankingproject/controller/ClientController.java
@@ -5,7 +5,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
-import java.io.IOException; // Importation de IOException
+import java.io.IOException;
 
 public class ClientController {
 
